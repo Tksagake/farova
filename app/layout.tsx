1f98a5e23297885761f1a6208f52589bf075d865
Sikolia Malcolm <malcolmsikolia@gmail.com>
2025-03-29 19:51:36 +0300

    MVP.1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,16 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOM8y8g7xj6z4Y2e1v5p5e5b5e5b5e5b5e5b5e5"
+          crossOrigin="anonymous"
+        />
 </head>
 
       <body
